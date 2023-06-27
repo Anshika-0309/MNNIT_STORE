7e8dac06aaeca496ab83b028cc879322bc4f2cac
Anshika <anshikaa0309@gmail.com>
2023-06-28 01:49:15 +0530

    done

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ODFmMDc4NTE4Yzk1ZWM3YjJmMWFkZSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4Njg0NDI1MywiZXhwIjoxNjg3MTAzNDUzfQ.xuw1n8nnlKOlBc6XdFOQogedQeiB6mHc-u8YK3QfPJ0";
+const TOKEN=JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL
